82995fb06f0a434f566c1efcc5c87f826b47863f
maznika <gmasnica@Garys-MacBook-Pro.local>
2016-02-06 20:34:42 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+This repo contains the exercises on http://cryptopals.com done in Python
---
+++ lib/hex_to_x.py
@@ -0,0 +1,20 @@
+import binascii
+import base64
+
+
+class HexToAscii(Object):
+    
+    def __init__(self):
+        print 'yay'
+
+
+class HexToBinary(Object):
+
+    def __init__(self):
+        print 'yay'
+
+class HexToBase64(Object):
+
+    def __init__(self):
+        print 'yay'
+
